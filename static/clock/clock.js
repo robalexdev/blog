c103aad2bf282aca13473c19c110193cbc402ae1
Robert Alexander <robert@robalexdev.com>
2026-04-02 18:36:48 -0400

    Clock: refactor

--- static/clock/clock.js
+++ static/clock/clock.js
@@ -1,201 +1,52 @@
-/**
-    (c) Robert Alexander
-    MIT License
-*/
-const atoz = "abcdefghijklmnopqrstuvwxyz";
-var uniqueId = "";
-while (uniqueId.length < 12) {
-    uniqueId += atoz[Math.floor(Math.random() * atoz.length)];
+// Static-hosted web clocks
+// (c) Robert Alexander 2026
+// License: MIT
+
+const httpClockElem = document.querySelector("#http-clock");
+const fastlyClockElem = document.querySelector("#fastly-clock");
+
+function updateClockDisplay(offset, div) {
+  return () => {
+    const local = new Date();
+    const now = new Date(local.getTime() - offset);
+    div.innerText = now.toLocaleTimeString();
+  };
 }
 
-const timestamps = {};
-const timings = {};
-const offsets = {};
-const serverDelay = {};
+async function loadClock(elem, fn) {
+  if (!elem) return;
+  const timeDiv = elem.querySelector(".the-time");
+  const offsetDiv = elem.querySelector(".the-offset");
+  const accuracyDiv = elem.querySelector(".the-accuracy");
+  const timelineDiv = elem.querySelector(".the-timeline");
 
-// High precision network request timing
-const resourceObserver = new PerformanceObserver((p) => {
-  for (const entry of p.getEntries()) {
-    if (entry.name.includes("/does-not-exist-")) {
-      timings['http'] = [
-        entry.requestStart,
-        entry.responseStart,
-      ];
-    } else if (entry.name.includes("/zero")) {
-      timings['fastly'] = [
-        entry.requestStart,
-        entry.responseStart,
-      ];
-    }
-  }
-  if (timings.length >= 2) {
-    resourceObserver.disconnect();
-  }
-});
-resourceObserver.observe({ type: "resource" });
+  const [ timing, offset, error ] = await fn();
 
-const fetchAndTimeHeader = function(url, headerName) {
-  return fetch(
-    new Request(url),
-    { method: "HEAD" },
-  ).then(
-    async (response) => response.headers.get(headerName)
-  );
-};
-
-/**
-
-    HTTP Date header clock synchronization
-    This will only be accurate if:
-    - the server provides the current time on 404 Error
-    - the server actually has the right time
-    Ideally, the server should know it is acting as a time server and provide SLAs
-*/
-
-const outputDiv = document.querySelector("#the-time");
-const offsetDiv = document.querySelector("#the-offset");
-const accuracyDiv = document.querySelector("#the-accuracy");
-
-fetchAndTimeHeader("does-not-exist-" + uniqueId, "date").then((tsHeader) => {
-  var timestamp = new Date(tsHeader);
-  // The timestamp is rounded downward, add 500ms to compensate
-  timestamps['http'] = new Date(timestamp.getTime() + 500);
-  return;
-});
-
-const httpWait = function() {
-  const timing = timings['http'];
-  const timestamp = timestamps['http'];
-
-  if (timing !== undefined && timestamp !== undefined) {
-    const start = performance.timeOrigin + timing[0];
-    const end   = performance.timeOrigin + timing[1];
-
-    const rtt = end - start;
-    const halfRtt = rtt / 2;
-    const errorRounding = 500;
-
-    // A guess for the maximum clock offset a GitHub Pages web server can have
-    const errorServer = 100;
-    const errorNetwork = halfRtt;
-    const errorSeconds = ((errorRounding + errorServer + errorNetwork) / 1000).toFixed(1);
-
-    // Note: NTP also accounts for server delay, but we consider that part of network delay
-    const offset = end - timestamp - halfRtt;
-    offsets['http'] = offset;
-
-    var offsetText = "synchronized";
-    if (offset < -1000 * errorSeconds) {
-        offsetText = Math.abs(offset/1000).toFixed(1) + " seconds behind";
-    } else if (offset > 1000 * errorSeconds) {
-        offsetText = Math.abs(offset/1000).toFixed(1) + " seconds ahead";
-    }
-
-    offsetDiv.innerText = "Your device's clock is " + offsetText;
-    accuracyDiv.innerText = "Calculated offset is " + (offset/1000).toFixed(1) + " ±" + errorSeconds + " seconds";
-    setInterval(() => {
-      const local = new Date();
-      const now = new Date(local.getTime() + offset);
-      outputDiv.innerText = now.toLocaleTimeString();
-    }, 50);
-  } else {
-    // Try again
-    setTimeout(httpWait, 80);
-  }
-};
-
-setTimeout(httpWait, 80);
-
-
-/**
-    High precision clock synchronization using Fastly X-Timer
-
-    Uses an empty file which will ideally remain in the cache
-    Ideally, Fastly should acknowledge it's role as a time server and provide SLAs
-    before this is used for anything serious
-*/
-const output2Div = document.querySelector("#the-time-2");
-const offset2Div = document.querySelector("#the-offset-2");
-const accuracy2Div = document.querySelector("#the-accuracy-2");
-
-fetchAndTimeHeader("zero?r=" + uniqueId, "x-timer").then((tsHeader) => {
-  // I.E.: S1773690070.994529,VS0,VE6
-  if (!tsHeader) {
-    output2Div.innerText = "Failed: no x-timer header";
-    return;
-  }
-
-  var parts = tsHeader.split(',');
-  var unixStartTimeSeconds = 0;
-  var durationMilliseconds = 0;
-  for (part of parts) {
-    if (part.startsWith("S")) {
-      unixStartTimeSeconds = Number(part.slice(1));
-    } else if (part.startsWith("VE")) {
-      durationMilliseconds = Number(part.slice(2));
+  if (!!timeDiv) {
+    if (!!offset) {
+      setInterval(updateClockDisplay(offset, timeDiv), 50);
+    } else {
+      timeDiv.innerText = "Loading failed";
     }
   }
 
-  serverDelay['fastly'] = durationMilliseconds;
-  timestamps['fastly'] = new Date(unixStartTimeSeconds*1000);
-  return;
-});
-
-const fastlyWait = function() {
-  const timing = timings['fastly'];
-  const timestamp = timestamps['fastly'];
-  const durationMilliseconds = serverDelay['fastly'];
-
-  if (timing !== undefined && timestamp !== undefined) {
-    const start = performance.timeOrigin + timing[0];
-    const end   = performance.timeOrigin + timing[1];
-
-    const rtt = end - start;
-    const halfRtt = rtt / 2;
-
-    // A guess for the maximum clock offset a Fastly server can have
-    const errorServer = 100;
-    const errorNetwork = halfRtt;
-    const errorSeconds = ((errorServer + errorNetwork) / 1000).toFixed(1);
-    const offset = end - timestamp + durationMilliseconds - halfRtt;
-    offsets['fastly'] = offset;
-
-    var offsetText = "synchronized";
-    if (offset < -1000 * errorSeconds) {
-        offsetText = Math.abs(offset/1000).toFixed(1) + " seconds behind";
-    } else if (offset > 1000 * errorSeconds) {
-        offsetText = Math.abs(offset/1000).toFixed(1) + " seconds ahead";
+  if (!!offsetDiv && !!offset) {
+    if (Math.abs(offset) > error) {
+      if (offset < 0) {
+        offsetDiv.innerText = "Your system clock is behind by " + Math.abs(offset/1000).toFixed(3) + " seconds";
+      } else {
+        offsetDiv.innerText = "Your system clock is ahead by " + (offset/1000).toFixed(3) + " seconds";
+      }
+    } else {
+      offsetDiv.innerText = "Your system clock is synchronized";
     }
-
-    offset2Div.innerText = "Your device's clock is " + offsetText;
-    accuracy2Div.innerText = "Calculated offset is " + (offset/1000).toFixed(1) + " ±" + errorSeconds + " seconds";
-
-    setInterval(() => {
-        const local = new Date();
-        const now = new Date(local.getTime() + offset);
-        output2Div.innerText = now.toLocaleTimeString();
-    }, 50);
-  } else {
-    // Try again
-    setTimeout(fastlyWait, 80);
   }
-};
 
-setTimeout(fastlyWait, 80);
-
-const offsetComparedDiv = document.querySelector("#the-delta");
-const bothWait = function() {
-  const httpOffset = offsets['http'];
-  const fastlyOffset = offsets['fastly'];
-
-  if (httpOffset !== undefined && fastlyOffset !== undefined) {
-    offsetComparedDiv.innerText = "Offsets are " + (httpOffset - fastlyOffset)/1000 + " seconds apart";
-  } else {
-    setTimeout(bothWait, 100);
+  if (!!accuracyDiv && error !== null) {
+    accuracyDiv.innerText = "+/- " + (error/1000).toFixed(3) + " seconds";
   }
-};
-
-if (offsetComparedDiv) {
-  setTimeout(bothWait, 100);
 }
+
+loadClock(httpClockElem, estimateSystemClockOffsetUsingHTTP);
+loadClock(fastlyClockElem, estimateSystemClockOffsetUsingXTimer);
 
---
+++ static/clock/offset.js
@@ -0,0 +1,132 @@
+// Static-hosted clock offset estimation
+// (c) Robert Alexander 2026
+// License: MIT
+
+function dbg(msg) {
+  const d = document.querySelector("#debug");
+  if (!d) return;
+  const p = document.createElement("p");
+  p.innerText = msg;
+  d.appendChild(p);
+}
+
+// Network request timer
+const resourceTimingCallbacks = {};
+const resourceObserver = new PerformanceObserver((p) => {
+  for (const entry of p.getEntries()) {
+    for (const [name, cb] of Object.entries(resourceTimingCallbacks)) {
+      if (entry.name.includes(name)) {
+        dbg("Got timing information for " + entry.name);
+        cb(entry);
+      }
+    }
+  }
+});
+resourceObserver.observe({ type: "resource" });
+
+function done() {
+  resourceObserver.disconnect();
+}
+
+const fetchTimeHeader = function(url, headerName) {
+  dbg("Fetching " + url + " to get " + headerName + " header");
+  return fetch(
+    new Request(url),
+    { method: "HEAD" },
+  ).then(
+    async (response) => response.headers.get(headerName)
+  );
+};
+
+function uniq() {
+  const atoz = "abcdefghijklmnopqrstuvwxyz";
+  var uniqueId = "";
+  while (uniqueId.length < 12) {
+      uniqueId += atoz[Math.floor(Math.random() * atoz.length)];
+  }
+  return uniqueId;
+}
+
+async function estimateSystemClockOffset(pathPrefix, headerName, fHeader) {
+  const path = pathPrefix + uniq();
+  const waitForTiming = new Promise((resolve, reject) => {
+    resourceTimingCallbacks[path] = resolve;
+  });
+  const [serverTimestamp, delay, extraError] = fHeader(await fetchTimeHeader(path, headerName));
+  dbg("Server Timestamp: " + serverTimestamp);
+  if (serverTimestamp === null) {
+    return [null, null, null];
+  }
+  const timing = await waitForTiming;
+  const [offset, error] = calculateOffset(timing, serverTimestamp, delay, extraError);
+  return [timing, offset, error];
+}
+
+function calculateOffset(timing, serverTimestamp, serverDelay, extraError) {
+  // Get the current time from both the performance clock and the system clock
+  // These are usually aligned, but not always
+  const systemTimeReferenceMs = new Date().getTime();
+  const perfTimeReferenceMs = performance.now();
+
+  // Walk the system clock back to the request end timestamp
+  // This is extra delay added by the performance callback
+  const callbackDelayMs = perfTimeReferenceMs - timing.responseEnd;
+  const systemTimeAtRequestEndMs = systemTimeReferenceMs - callbackDelayMs;
+
+  //
+  // Estimate how long before request end the server generated the timestamp
+  // This assumes network delay is symmetric
+  //
+  const rtt = timing.responseEnd - timing.requestStart;
+  const halfRtt = rtt / 2;
+
+  // The Fastly timestamp is taken at the start of the server processing time
+  // So walk back by an extra half the server processing time
+  const offset = systemTimeAtRequestEndMs - serverTimestamp - halfRtt - (serverDelay/2);
+  const error = halfRtt + extraError;
+
+  return [offset, error];
+}
+
+// See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Date
+async function estimateSystemClockOffsetUsingHTTP() {
+  const result = await estimateSystemClockOffset(
+    "does-not-exist-",
+    "date",
+    (ts) => { 
+      // The Date header timestamp is rounded downward
+      // add 500ms to compensate
+      const timestamp = new Date(ts).getTime() + 500;
+      const delay = 0; // No data provided
+      return [timestamp, delay, 500];
+    },
+  );
+  dbg("HTTP results: " + result);
+  return result;
+}
+
+// See: https://www.fastly.com/documentation/reference/http/http-headers/X-Timer/
+async function estimateSystemClockOffsetUsingXTimer() {
+  const result = await estimateSystemClockOffset(
+    "zero?r=",
+    "x-timer",
+    (headerValue) => { 
+      if (! headerValue) {
+        return [ null, null, null ];
+      }
+      var parts = headerValue.split(',');
+      var unixStartTimeSeconds = null;
+      var durationMilliseconds = null;
+      for (part of parts) {
+        if (part.startsWith("S")) {
+          unixStartTimeSeconds = Number(part.slice(1));
+        } else if (part.startsWith("VE")) {
+          durationMilliseconds = Number(part.slice(2));
+        }
+      }
+      return [ unixStartTimeSeconds*1000, durationMilliseconds, 1 ];
+    },
+  );
+  dbg("X-Timer results: " + result);
+  return result;
+}
